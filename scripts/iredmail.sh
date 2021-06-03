c2a6382b8568720b918c61a1c7de20123c4616f5
Max <max@debian-fx>
2021-06-03 18:21:50 +0200

    Added Post-configuration steps

--- scripts/iredmail.sh
+++ scripts/iredmail.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+function LOG() { echo -e "\e[32m$@\e[0m"; }
 
 # Downloads and then runs the iRedMail scripts for setting up a mailserver.
 # Use on Debian 10.
@@ -14,6 +15,10 @@ IREDMAIL_URL="https://github.com/iredmail/iRedMail/archive/${IREDMAIL_VERSION}.t
 
 ### END CONFIGURATION ###
 
+sudo apt-get install -y --no-install-recommends gnupg2
+
+cd /root
+
 # download iredmail
 filename="iRedMail-$(basename ${IREDMAIL_URL})"
 wget -O ${filename} ${IREDMAIL_URL}
@@ -21,6 +26,59 @@ wget -O ${filename} ${IREDMAIL_URL}
 # extract
 tar -xvf ${filename}
 
+# pre-configure some services to not require seperate namespaces:
+LOG "Pre-configuring services for container..."
+
+mkdir -p /etc/systemd/system/dovecot.service.d
+cat << EOF > /etc/systemd/system/dovecot.service.d/override.conf
+[Service]
+PrivateTmp=false
+ProtectSystem=false
+NoNewPrivileges=true
+PrivateDevices=false
+EOF
+
+mkdir -p /etc/systemd/system/mariadb.service.d
+cat << EOF > /etc/systemd/system/mariadb.service.d/override.conf
+[Service]
+PrivateTmp=false
+ProtectSystem=false
+NoNewPrivileges=true
+PrivateDevices=false
+EOF
+
+mkdir -p /etc/systemd/system/memcached.service.d
+cat << EOF > /etc/systemd/system/memcached.service.d/override.conf
+[Service]
+PrivateTmp=false
+ProtectSystem=false
+NoNewPrivileges=true
+PrivateDevices=false
+EOF
+
+mkdir -p /etc/systemd/system/memcached.service.d
+cat << EOF > /etc/systemd/system/memcached.service.d/override.conf
+[Service]
+PrivateTmp=false
+ProtectSystem=false
+NoNewPrivileges=true
+PrivateDevices=false
+EOF
+
+mkdir -p /etc/systemd/system/phpsessionclean.service.d
+cat << EOF > /etc/systemd/system/phpsessionclean.service.d/override.conf
+[Service]
+PrivateTmp=false
+ProtectSystem=false
+NoNewPrivileges=true
+PrivateDevices=false
+EOF
+
+
 # run setup script
 cd iRedMail-${IREDMAIL_VERSION}
 bash iRedMail.sh
+
+LOG
+LOG "Container setup ok. "
+LOG
