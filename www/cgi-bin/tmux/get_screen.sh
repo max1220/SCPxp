44ba96b84cc4f3805e753e3be41b49e791da1c5f
Max <max1220@users.noreply.github.com>
2023-02-04 02:23:29 +0100

    rename session_name parameter to target_name to more accurately reflect what it does(can be session or session:window or session:window.pane)

--- www/cgi-bin/tmux/get_screen.sh
+++ www/cgi-bin/tmux/get_screen.sh
@@ -15,8 +15,8 @@ parse_query_parms_list "$(</dev/stdin)"
 parse_query_parms_arr true
 
 # get the name of the session
-session_name="${query_parms_arr[session_name]:-}"
-[ "${session_name}" = "" ] && exit_with_status_message "400" "Bad request"
+target_name="${query_parms_arr[target_name]:-}"
+[ "${target_name}" = "" ] && exit_with_status_message "400" "Bad request"
 
 # enable escape sequences? This will also force base64-encoding
 enable_escape="${query_parms_arr[escape]:-}"
@@ -27,13 +27,13 @@ base64_encode="${query_parms_arr[base64_encode]:-}"
 # get the screen content
 screen_content=""
 if [ "${enable_escape}" = "true" ] && [ "${base64_encode}" = "true" ]; then
-	screen_content="$(tmux capture-pane -t "${session_name}" -p -e | base64 || exit_with_status_message "400" "Bad request")"
+	screen_content="$(TERM=ansi tmux capture-pane -t "${target_name}" -p -e | base64 || exit_with_status_message "400" "Bad request")"
 elif [ "${enable_escape}" = "true" ]; then
-	screen_content="$(tmux capture-pane -t "${session_name}" -p -e || exit_with_status_message "400" "Bad request")"
+	screen_content="$(TERM=ansi tmux capture-pane -t "${target_name}" -p -e || exit_with_status_message "400" "Bad request")"
 elif [ "${base64_encode}" = "true" ]; then
-	screen_content="$(tmux capture-pane -t "${session_name}" -p | base64 || exit_with_status_message "400" "Bad request")"
+	screen_content="$(TERM=ansi tmux capture-pane -t "${target_name}" -p | base64 || exit_with_status_message "400" "Bad request")"
 else
-	screen_content="$(tmux capture-pane -t "${session_name}" -p || exit_with_status_message "400" "Bad request")"
+	screen_content="$(TERM=ansi tmux capture-pane -t "${target_name}" -p || exit_with_status_message "400" "Bad request")"
 fi
 
 
