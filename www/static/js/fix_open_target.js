ffb0422e0f10c74c7f741326c379eab5050f0ab6
Max <max1220@users.noreply.github.com>
2023-02-08 20:04:30 +0100

    Fix for chromes open(url, target) not accepting a renamed iframe

---
+++ www/static/js/fix_open_target.js
@@ -0,0 +1,20 @@
+// fix the link target problem by overriding the onclick behaviour of <a> elements with a target= attribute set.
+function fixed_open(url, target, specs, replace) {
+	let iframe_elem
+	if (target) {
+		iframe_elem = document.getElementsByName(target)[0] || parent.document.getElementsByName(target)[0]
+	}
+	if (iframe_elem) {
+		console.log("fixed_open", url)
+		iframe_elem.src = url
+		return
+	} else {
+		return open(url, target, specs, replace)
+	}
+}
+
+// apply fix only for chrome browser
+if (window.chrome) {
+	// replace open function
+	open = fixed_open
+}
