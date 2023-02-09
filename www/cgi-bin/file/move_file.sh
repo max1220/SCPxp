35046bc0b21c1732099b036ea7b99fecfee6d023
Max <max1220@users.noreply.github.com>
2023-02-09 04:27:56 +0100

    Fixed move_file

--- www/cgi-bin/file/move_file.sh
+++ www/cgi-bin/file/move_file.sh
@@ -14,16 +14,14 @@ parse_query_parms_list "$(</dev/stdin)"
 # parse into array and URL-decode
 parse_query_parms_arr true
 
-# get the filepath arguments
+# get the source and target arguments
 source_file_path="${query_parms_arr[source_file_path]:-}"
-target_file_path="${query_parms_arr[target_file_path]:-}"
-[ "${source_file_path}" = "" ] && exit_with_status_message "400" "Bad request"
-[ "${target_file_path}" = "" ] && exit_with_status_message "400" "Bad request"
-
-# check that $source_file_path exists
+target_dir_path="${query_parms_arr[target_dir_path]:-}"
 [ -e "${source_file_path}" ] || exit_with_status_message "400" "Bad request"
+[ -d "${target_dir_path}" ] || exit_with_status_message "400" "Bad request"
 
-rm -f -- "${file_path}"
+# move file
+mv "${source_file_path}" "${target_dir_path}"
 
 # respond with success
 cat << EOF
