031997fbdead3053de045d50fcd25d94e4268cba
Max <max1220@users.noreply.github.com>
2023-02-12 04:32:49 +0100

    Support an error callback; more comments

--- www/static/js/make_xhr.js
+++ www/static/js/make_xhr.js
@@ -1,17 +1,25 @@
 // make a HTTP request, optionally call cb with the returned body
-function make_xhr(url, method, content_type, body, cb) {
+function make_xhr(url, method, content_type, body, cb, err_cb) {
 	let req = new XMLHttpRequest()
-	if (cb) {
-		req.onreadystatechange = function() {
-			if (this.readyState == 4 && this.status == 200) {
-				cb(url, this.responseText)
-			}
+
+	// register function to detect completion XHR
+	req.onreadystatechange = function() {
+		if (cb && (this.readyState) == 4 && (this.status == 200)) {
+			return cb(url, this.responseText, req)
+		} else if(err_cb && (this.readyState == 4)) {
+			return err_cb(url, this.status, this.responseText, req)
 		}
 	}
+
+	// configure the request
 	req.open(method, url)
 	if (content_type) {
 		req.setRequestHeader("Content-type", content_type)
 	}
+
+	// send the request(req.onreadystatechange will call callback on completion)
 	req.send(body)
+
+	// return request for convenience
 	return req
 }
