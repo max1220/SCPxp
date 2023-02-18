d1584c6dfb4a9bfcabb8cda511a4244bdc347cc8
Max <max1220@users.noreply.github.com>
2023-02-18 15:57:45 +0100

    Split make_xhr into async and sync versions

--- www/static/js/make_xhr.js
+++ www/static/js/make_xhr.js
@@ -1,8 +1,8 @@
-// make a HTTP request, optionally call cb with the returned body
-function make_xhr(url, method, content_type, body, cb, err_cb) {
+// run a XMLHttpRequest
+function make_xhr_async(url, method, content_type, body, cb, err_cb) {
 	let req = new XMLHttpRequest()
 
-	// register function to detect completion XHR
+	// on request completion call cb/err_cb
 	req.onreadystatechange = function() {
 		if (cb && (this.readyState) == 4 && (this.status == 200)) {
 			return cb(url, this.responseText, req)
@@ -11,15 +11,29 @@ function make_xhr(url, method, content_type, body, cb, err_cb) {
 		}
 	}
 
-	// configure the request
-	req.open(method, url)
-	if (content_type) {
-		req.setRequestHeader("Content-type", content_type)
-	}
+	// set content-type header if any
+	if (content_type) { req.setRequestHeader("Content-type", content_type); }
 
-	// send the request(req.onreadystatechange will call callback on completion)
+	// start the request
+	req.open(method, url, true)
 	req.send(body)
 
 	// return request for convenience
 	return req
+}
+// run a XMLHttpRequest synchronously, return responseText
+function make_xhr_sync(url, method, content_type, body) {
+	let req = new XMLHttpRequest()
+
+	// set content-type header if any
+	if (content_type) { req.setRequestHeader("Content-type", content_type); }
+
+	// run the request
+	req.open(method, url, false)
+	req.send(body)
+
+	// return request body if successful
+	if (req.status == 200) {
+		return req.responseText
+	}
 }
