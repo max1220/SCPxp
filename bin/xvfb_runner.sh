7010ffce1c2b55c3a6e2ba95ed1150216747d82e
Max <max1220@users.noreply.github.com>
2023-02-18 16:02:19 +0100

    New session script for xvfb-run

--- bin/xvfb_runner.sh
+++ bin/xvfb_runner.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
-mkdir -p xsessions
+# This is a utillity script used by the X11 streamer web application to
+# create a session based on a few arguments. See x11_streamer.js.
 
-# create file with info about display
-RANDOM_ID="${1}"
-echo -n "${DISPLAY}" > "xsessions/${RANDOM_ID}.display"
-shift
+# make sure the session directory exists
+SESSION_DIR="xsessions/${SESSION_ID}"
+mkdir -p "${SESSION_DIR}"
 
-# launch the window manager
-ratpoison &
+# create some info files
+echo -n "${BASHPID}" > "${SESSION_DIR}/pid"
+echo -n "${DISPLAY}" > "${SESSION_DIR}/display"
+
+# create a fifo to write commands to xdotool
+mkfifo "${SESSION_DIR}/xdotool"
+xdotool "${SESSION_DIR}/xdotool" &
+exec 3>"${SESSION_DIR}/xdotool" # keep EOF from reaching xdotool
+
+# start the window manager if requested
+if ! [ "${SESSION_WM:-}" = "" ]; then
+	"${SESSION_WM[@]}" &
+fi
 
 # start the command
 "${@}"
