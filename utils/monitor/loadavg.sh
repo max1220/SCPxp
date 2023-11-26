b400d3b6d55d3d621dc171877fc1fc11989be809
max1220 <max1220@users.noreply.github.com>
2023-11-26 14:23:04 +0100

    removed more old stuff

--- utils/monitor/loadavg.sh
+++
@@ -1,8 +0,0 @@
-function monitor_loadavg() {
-	local delay="${1}"
-	while sleep "${delay}"; do
-		read -r loadavg rest < /proc/loadavg
-		echo "${loadavg}"
-		true;
-	done | csv_add_timestamps
-}
--- utils/timestamp.sh
+++
@@ -1,6 +0,0 @@
-
-# get time in seconds since epoch
-function get_timestamp() {
-	#date +%s.%N
-	echo "${EPOCHREALTIME}"
-}
